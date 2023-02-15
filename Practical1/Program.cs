b7690daf99fde7e5c061a4bcb3c5e868accfe027
darshit73 <124660502+darshit73@users.noreply.github.com>
2023-02-15 18:21:10 +0530

    init

--- Practical1/Program.cs
+++ Practical1/Program.cs
@@ -1,69 +1,106 @@
 ﻿
-
 using System.Data;
 using System.Data.SqlClient;
-using System.Diagnostics;
+using Practical1;
 
 class Program
 {
-    private static string connectionString =
-        "Data Source=DESKTOP-1VJLI7P;Initial Catalog=practical1db;Integrated Security=True";
-    private  static string createTableQuery = "CREATE TABLE SourceTable(ID INT IDENTITY(1,1) PRIMARY KEY, FirstNumber INT, SecondNumber INT)";
-    static readonly SqlConnection connection = new SqlConnection(connectionString);
+    private static string connectionString = "Data Source=DESKTOP-1VJLI7P;Initial Catalog=practical1db;Integrated Security=True";
+    private  static string createSourceTableQuery = "CREATE TABLE SourceTable(ID INT IDENTITY(1,1) PRIMARY KEY, FirstNumber INT, SecondNumber INT)";
+    private static string createDestinationTableQuery = "CREATE TABLE DestinationTable(ID INT IDENTITY(1,1) PRIMARY KEY,Fkey INT, Sum INT, FOREIGN KEY (Fkey) REFERENCES SourceTable(ID))";
+    static readonly SqlConnection Connection = new (connectionString);
+    private static int totalData = 1000;
+    private static int batchSize = 100;
+    private static int _currentIndex;
+    private static int _completed;
+    static int endNumber =0;
+    static Thread thread;
+    static int startNumber;
+    private static bool createSourceTable = false;
+    private static bool createDestinationTable = false;
 
-    static void Main(string[] args)
+    static void Main()
     {
-      
-        CreateSourceTable();
-        CreateDestinationTable();
+        Connection.Open();
+        start:
+        Console.WriteLine("1. Create SourceTable\n" +
+                          "2. Migrate Data from Source To Destination Table\n" +
+                          "3. Status of your Migration\n" +
+                          "4. Cancel Migration\n" +
+                          "5. Exit");
+        
+        Console.WriteLine("Enter one choice");
+        int switchNum = Convert.ToInt32(Console.ReadLine());
+        
+        switch (switchNum)
+        {
+            case 1 :
+                if (!createSourceTable)
+                {
+                    createSourceTable = true;
+                    CreateSourceTable();
+                }
+                else
+                {
+                    Console.WriteLine("Table is Already Created");
+                }
+                break;
+            case 2:
+                if (createSourceTable)
+                {
+                    Console.WriteLine($"Total Number of record in source table is {totalData} how many you want to Migrate");
+                    Console.WriteLine("Enter a startNumber : ");
+                    startNumber = Convert.ToInt32(Console.ReadLine());
+
+                    Console.WriteLine("Enter a endNumber : ");
+                    endNumber = Convert.ToInt32(Console.ReadLine());
+                    thread = new Thread((() => CreateDestinationTable(startNumber,endNumber)));
+                    thread.Start();
+                }
+                else
+                {
+                    Console.WriteLine("Please create Source Table First");
+                }
+                break;
+            case 3 :
+                if (createSourceTable && createDestinationTable)
+                {
+                    Console.WriteLine($"Total number of data to insert is {endNumber - startNumber+1}\n " +
+                                      $"Data added in Destination is {_completed}\n" +
+                                      $"Data that left is {endNumber - startNumber - _completed+1}\n");
+                }
+                else
+                {
+                    Console.WriteLine("first create tables");
+                }
+                break;
+            case 4:
+                thread.Interrupt();
+                break;
+            case 5: 
+                break;
+            default:
+                Console.WriteLine("Please Enter Valid Choies");
+                break;
+        }
+
+        Console.WriteLine();
+        if (switchNum != 5)
+        {
+            goto start;
+        }
     }
 
-    private static void CreateDestinationTable()
-    {   
-             List<MigrationRecord> migrationRecords = new List<MigrationRecord>();
-             for (int i = 0; i<=100000; i++)
-             {
-                 MigrationRecord record = ReadMigrationRecord(i);
-                 if (record != null)
-                 {
-                     migrationRecords.Add(record);
-                 }
-             }
-             
-             if (migrationRecords.Count > 0)
-             {
-                 using (SqlConnection connection = new SqlConnection(connectionString))
-                 {
-                     connection.Open();
-
-                     SqlCommand command = new SqlCommand("INSERT INTO DestinationTable (ID, Sum) VALUES (@ID, @Sum)", connection);
-                     command.Parameters.AddWithValue("@ID", 0);
-                     command.Parameters.AddWithValue("@Sum", 0);
-
-                     foreach (MigrationRecord record in migrationRecords)
-                     {
-                         command.Parameters["@ID"].Value = record.Id;
-                         command.Parameters["@Sum"].Value = record.Sum;
-
-                         command.ExecuteNonQuery();
-                         connection.Close();
-                     }
-                 }
-             }
-    }
-
-    private static void CreateSourceTable()
+      private static void CreateSourceTable()
     {
-        connection.Open();
-        SqlCommand sqlCommand = new SqlCommand(createTableQuery, connection);
+        SqlCommand sqlCommand = new SqlCommand(createSourceTableQuery, Connection);
         sqlCommand.ExecuteNonQuery();
-        connection.Close();
         
         DataTable tbl = new DataTable();
         tbl.Columns.Add(new DataColumn("FirstNumber", typeof(Int32)));  
         tbl.Columns.Add(new DataColumn("SecondNumber", typeof(Int32)));  
       
-        for(int i=0; i<10000; i++)  
+        for(int i=0; i<totalData; i++)  
         {   
             DataRow dr = tbl.NewRow();
             dr["FirstNumber"] = i;
@@ -71,49 +108,66 @@ class Program
             tbl.Rows.Add(dr);   
         }  
         
-        SqlBulkCopy objbulk = new SqlBulkCopy(connection);
+        SqlBulkCopy objbulk = new SqlBulkCopy(Connection);
         objbulk.DestinationTableName = "SourceTable";  
         
         
         objbulk.ColumnMappings.Add("FirstNumber", "FirstNumber");   
         objbulk.ColumnMappings.Add("SecondNumber", "SecondNumber");
         
-        Stopwatch timer = new Stopwatch();
-        timer.Start();
+      
         
-        connection.Open();
-        objbulk.WriteToServer(tbl);  
-        connection.Close();  
+        objbulk.WriteToServer(tbl);
         
-        timer.Stop();
-        Console.WriteLine($"Time elapsed: {timer.ElapsedMilliseconds}");
-
-       
-        // time taken to add data using for loop for 1M data it take more than 1 minute
-        //using above bulkCopy methode take 0.8 sec .
-           
-            // Loop to insert one million records into the database
-            // for (int i = 1; i <= 1000000; i++)
-            // {
-            //     // Create a SQL command object
-            //     using SqlCommand command = new SqlCommand(
-            //         "INSERT INTO SourceTable(FirstNumber, SecondNumber) VALUES (@param1, @param2)", connection);
-            //     // Add the parameters to the command object
-            //     command.Parameters.AddWithValue("@param1", i);
-            //     command.Parameters.AddWithValue("@param2", i * 2);
-            //     // Execute the SQL command
-            //     command.ExecuteNonQuery();
-            // }
-            // connection.Close();
         
-     
     }
-    static MigrationRecord ReadMigrationRecord(int id)
+      private static void CreateDestinationTable(int startNumber,int endNumber)
     {
-        MigrationRecord record = null;
-        connection.Open();
+        SqlCommand sqlCommand = new SqlCommand(createDestinationTableQuery, Connection);
+        sqlCommand.ExecuteNonQuery();
+        _currentIndex = startNumber;
+        
+        while (_currentIndex<=endNumber)
+        {
+            List<Record> migrationRecords = new List<Record>();
+            int temp=_currentIndex;
+            for (int i = 0; i < batchSize && _currentIndex <= endNumber; i++)
+            {
+                Record record = CheckRecordInSourceTable(_currentIndex);
+                if (record != null)
+                {
+                    migrationRecords.Add(record);
+                    _currentIndex++;
+                }
+            }
+            
 
-            SqlCommand command = new SqlCommand($"SELECT * FROM SourceTable WHERE ID = {id}", connection);
+            if (migrationRecords.Count > 0)
+            {
+                SqlCommand command = new SqlCommand("INSERT INTO DestinationTable (Fkey, Sum) VALUES (@Fkey, @Sum)", Connection);
+                command.Parameters.AddWithValue("@Fkey", 0);
+                command.Parameters.AddWithValue("@Sum", 0);
+                foreach (Record record in migrationRecords)
+                {
+                    command.Parameters["@Fkey"].Value = record.Id;
+                    command.Parameters["@Sum"].Value = record.Sum;
+                    command.ExecuteNonQuery();
+                    _completed++;
+                }
+            }
+
+            Console.WriteLine($"total number of record added is : {_completed} from {temp} to {_currentIndex-1}");
+          
+        }
+        Connection.Close();
+    }
+
+  
+    static Record CheckRecordInSourceTable(int id)
+    {
+             Record record = null;
+        
+            SqlCommand command = new SqlCommand($"SELECT * FROM SourceTable WHERE ID = {id}", Connection);
 
             using (SqlDataReader reader = command.ExecuteReader())
             {
@@ -122,25 +176,22 @@ class Program
                     int firstNumber = (int)reader["FirstNumber"];
                     int secondNumber = (int)reader["SecondNumber"];
 
-                    int sum = firstNumber + secondNumber;
-                    record = new MigrationRecord(id, sum);
+                    int sum = CalculateSum(firstNumber,secondNumber);
+                    record = new Record(id, sum);
                 }
             }
-
+           
+          
             return record;
+    }
+
+    static int CalculateSum(int firstNumber, int secondNumber)
+    {
+        Thread.Sleep(50);
+        return firstNumber + secondNumber;
     }
     
 }
-class MigrationRecord
-{
-    public int Id { get; set; }
-    public int Sum { get; set; }
 
-    public MigrationRecord(int id, int sum)
-    {
-        Id = id;
-        Sum = sum;
-    }
-}
 
 
---
+++ Practical1/Record.cs
@@ -0,0 +1,13 @@
+﻿namespace Practical1;
+
+public class Record
+{
+    public int Id { get; set; }
+    public int Sum { get; set; }
+
+    public Record(int id, int sum)
+    {
+        Id = id;
+        Sum = sum;
+    }
+}
